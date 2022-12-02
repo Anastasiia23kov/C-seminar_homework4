5d1571b471fa7b9f097c5c506491aebb11ac5b10
Anastasiia <penkova23anastasia@gmail.com>
2022-12-02 14:37:20 +0200

    Задача 29. Программа задает массив из 8 элементов и выводит их на экран.

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29. Программа задает массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+
+int [] array = new int [8];
+for(int i=0; i<array.Length; i++)
+{
+    Console.Write($"Введите элемент {i+1} массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
